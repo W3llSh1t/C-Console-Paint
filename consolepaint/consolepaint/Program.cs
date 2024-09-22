b6531348d9417e596d7c8dcc2e521a3ff3a420b3
Lehel Dániel Zsolt <114480078+W3llSh1t@users.noreply.github.com>
2024-09-22 20:52:33 +0200

    Újradolgozott

--- consolepaint/consolepaint/Program.cs
+++ consolepaint/consolepaint/Program.cs
@@ -4,6 +4,8 @@ namespace consolepaint
 {
     internal class Program
     {
+        const int startRow = 11;
+        const int startCol = 1;
         static void Main(string[] args)
         {
             Console.CancelKeyPress += (sender, e) =>
@@ -12,14 +14,61 @@ namespace consolepaint
                 Console.WriteLine("Exiting...");
                 Environment.Exit(0);
             };
+
+            int winwidth = Console.WindowWidth;
+            int winheight = Console.WindowHeight;
+            int row = 0;
+            int col = 0;
+            
+
             Console.WriteLine("Console Paint");
-            Console.WriteLine("-----------------------------------");
+            for (int i = 0; i < winwidth; i++)
+            {
+                Console.Write("-");
+            }
             Console.WriteLine("Controls:");
             Console.WriteLine("Moving between cells: ArrowKeys");
             Console.WriteLine("Color: 1,2,3,4");
             Console.WriteLine("Enter: SpaceBar");
             Console.WriteLine("Press ESC to Exit");
-            Console.WriteLine($"Current cell: 0;0");
+            
+            for (int i = 0; i < winwidth; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("Current Color: Red");
+            Console.WriteLine("Current saturation: █");
+
+            Console.Write("+");
+
+            for (int i = 0; i < (winwidth - 2); i++)
+            {
+                Console.Write("-");
+                col++;
+            }
+            Console.Write("+");
+
+
+
+            for (int i = 0; i < winheight - 9; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < winwidth - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.WriteLine();
+                row++;
+
+            }
+            Console.Write("+");
+            for (int i = 0; i < (winwidth - 2); i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.SetCursorPosition(startCol, startRow);
 
             var taskKeys = new Task(ReadKeys);
 
@@ -28,168 +77,158 @@ namespace consolepaint
             var tasks = new[] { taskKeys };
             Task.WaitAll(tasks);
         }
-        private static void ReadKeys()
+        public static void ReadKeys()
         {
             ConsoleKeyInfo key = new ConsoleKeyInfo();
-
-            string[,] table = new string[10, 10];
-            int row = 0;
-            int col = 0;
-            int wrow = 0;
-            int wcol = 0;
-            int i = 0;
-            int j = 0;
-            string write = " ";
-            int writenum = 0;
+            int currentRow = startRow;
+            int currentCol = startCol;
+            int color = 1;
+            int saturation = 1;
 
             while (!Console.KeyAvailable && key.Key != ConsoleKey.Escape)
             {
                 key = Console.ReadKey(true);
 
+                if(color == 1)
+                { 
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else if(color == 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else if(color == 3)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else if(color == 4)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+
                 switch (key.Key)
                 {
                     case ConsoleKey.UpArrow:
-                        if (row == 0)
+                        if (currentRow > startRow)
                         {
-
+                            currentRow--;
+                            Console.SetCursorPosition(currentCol, currentRow);
                         }
-                        else
-                        {
-                            row--;
-                        }
-                        MenuWrite();
-                        Console.WriteLine($"Current cell: {col};{row}");
-                        Console.WriteLine($"Current color: {writenum}  {write}");
                         break;
                     case ConsoleKey.DownArrow:
-                        if (row == 10)
+                        if (currentRow < Console.WindowHeight - 2)
                         {
-
+                            currentRow++;
+                            Console.SetCursorPosition(currentCol, currentRow);
                         }
-                        else
-                        {
-                            row++;
-                        }
-                        MenuWrite();
-                        Console.WriteLine($"Current cell: {col};{row}");
-                        Console.WriteLine($"Current color: {writenum}  {write}");
                         break;
 
                     case ConsoleKey.RightArrow:
-                        if (col == 10)
+                        if (currentCol < Console.WindowWidth - 2)
                         {
-
+                            currentCol++;
+                            Console.SetCursorPosition(currentCol, currentRow);
                         }
-                        else
-                        {
-                            col++;
-                        }
-                        MenuWrite();
-                        Console.WriteLine($"Current cell: {col};{row}");
-                        Console.WriteLine($"Current color: {writenum}  {write}");
                         break;
 
                     case ConsoleKey.LeftArrow:
-                        if (col == 0)
+                        if (currentCol > startCol)
                         {
-
+                            currentCol--;
+                            Console.SetCursorPosition(currentCol, currentRow);
                         }
-                        else
-                        {
-                            col--;
-                        }
-                        MenuWrite();
-                        Console.WriteLine($"Current cell: {col};{row}");
-                        Console.WriteLine($"Current color: {writenum}  {write}");
                         break;
 
                     case ConsoleKey.D1:
-                        write = "█";
-                        writenum = 1;
-                        MenuWrite();
-                        Console.WriteLine($"Current cell: {col};{row}");
-                        Console.WriteLine($"Current color: {writenum}  {write}");
+                        Console.ResetColor();
+                        Console.SetCursorPosition(startCol - 1,startRow - 3);
+                        Console.WriteLine("Current color: Red   ");
+                        Console.SetCursorPosition(currentCol, currentRow);
+                        color = 1;
                         break;
 
                     case ConsoleKey.D2:
-                        write = "▓";
-                        writenum = 2;
-                        MenuWrite();
-                        Console.WriteLine($"Current cell: {col};{row}");
-                        Console.WriteLine($"Current color: {writenum}  {write}");
+                        Console.ResetColor();
+                        Console.SetCursorPosition(startCol - 1, startRow - 3);
+                        Console.WriteLine("Current color: Blue  ");
+                        Console.SetCursorPosition(currentCol, currentRow);
+                        color = 2;
                         break;
 
                     case ConsoleKey.D3:
-                        write = "▒";
-                        writenum = 3;
-                        MenuWrite();
-                        Console.WriteLine($"Current cell: {col};{row}");
-                        Console.WriteLine($"Current color: {writenum}  {write}");
+                        Console.ResetColor();
+                        Console.SetCursorPosition(startCol - 1, startRow - 3);
+                        Console.WriteLine("Current color: Green ");
+                        Console.SetCursorPosition(currentCol, currentRow);
+                        color = 3;
                         break;
 
                     case ConsoleKey.D4:
-                        write = "░";
-                        writenum = 4;
-                        MenuWrite();
-                        Console.WriteLine($"Current cell: {col};{row}");
-                        Console.WriteLine($"Current color: {writenum}  {write}");
+                        Console.ResetColor();
+                        Console.SetCursorPosition(startCol - 1, startRow - 3);
+                        Console.WriteLine("Current color: Yellow");
+                        Console.SetCursorPosition(currentCol, currentRow);
+                        color = 4;
+                        break;
+                    
+                    case ConsoleKey.Q:
+                        Console.ResetColor();
+                        Console.SetCursorPosition(startCol - 1, startRow - 2);
+                        Console.WriteLine("Current saturation: █");
+                        Console.SetCursorPosition(currentCol, currentRow);
+                        saturation = 1;
+                        break;
+
+                    case ConsoleKey.W:
+                        Console.ResetColor();
+                        Console.SetCursorPosition(startCol - 1, startRow - 2);
+                        Console.WriteLine("Current saturation: ▓");
+                        Console.SetCursorPosition(currentCol, currentRow);
+                        saturation = 2;
+                        break;
+
+                    case ConsoleKey.E:
+                        Console.ResetColor();
+                        Console.SetCursorPosition(startCol - 1, startRow - 2);
+                        Console.WriteLine("Current saturation: ▒");
+                        Console.SetCursorPosition(currentCol, currentRow);
+                        saturation = 3;
+                        break;
+
+                    case ConsoleKey.R:
+                        Console.ResetColor();
+                        Console.SetCursorPosition(startCol - 1, startRow - 2);
+                        Console.WriteLine("Current saturation: ░");
+                        Console.SetCursorPosition(currentCol, currentRow);
+                        saturation = 4;
                         break;
 
                     case ConsoleKey.Spacebar:
-                        MenuWrite();
-                        Console.WriteLine($"Current cell: {col};{row}");
-                        Console.WriteLine($"Current color: {writenum}  {write}");
-                        table[row, col] = write;
-                        j++;
+                        if(saturation == 1)
+                        {
+                            Console.Write("█");
+                        }
+                        else if(saturation == 2)
+                        {
+                            Console.Write("▓");
+                        }
+                        else if(saturation == 3)
+                        {
+                            Console.Write("▒");
+                        }
+                        else if(saturation == 4)
+                        {
+                            Console.Write("░");
+                        }
+                        Console.SetCursorPosition(currentCol, currentRow);
                         break;
 
                     case ConsoleKey.Escape:
+                        Console.ResetColor();
+                        Console.SetCursorPosition(0, Console.WindowHeight + 3);
                         break;
                 }
-                if (j != 0)
-                {
-                    while (i < table.Length)
-                    {
-                        if (wcol != 10)
-                        {
-                            if (table[wrow, wcol] == "█" | table[wrow, wcol] == "▓" | table[wrow, wcol] == "▒" | table[wrow, wcol] == "░")
-                            {
-                                Console.Write(table[wrow, wcol]);
-                                wcol++;
-                                i++;
-                            }
-                            else
-                            {
-                                Console.Write(" ");
-                                wcol++;
-                                i++;
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine();
-                            wcol = 0;
-                            wrow++;
-                        }
-                    }
-                }
-                i = 0;
-                j = 0;
-                wcol = 0;
-                wrow = 0;
             }
-        }
-        private static void MenuWrite()
-        {
-            Console.Clear();
-            Console.WriteLine("Console Paint");
-            Console.WriteLine("-----------------------------------");
-            Console.WriteLine("Controls:");
-            Console.WriteLine("Moving between cells: ArrowKeys");
-            Console.WriteLine("Color: 1,2,3,4");
-            Console.WriteLine("Enter: SpaceBar");
-            Console.WriteLine("Press ESC to Exit");
-
         }
     }
 }
